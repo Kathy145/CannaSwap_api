90a4b0c0d96bb7d943e83df3cae7c79fa9b9995b
Kathy145 <Kathy.Lambert@LotusDesignsAgency.com>
2018-02-22 22:16:46 -0500

    Switch from SQlite to Postgresql and seeded the db

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 20180222170639) do
 
-  create_table "posts", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "posts", id: :serial, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
@@ -22,7 +25,7 @@ ActiveRecord::Schema.define(version: 20180222170639) do
     t.integer "rating"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :serial, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
